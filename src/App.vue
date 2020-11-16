8e0edf232eb18b1f2be989977298f3f003d3410c
wesley93 <1124700977@qq.com>
2020-11-17 00:37:37 +0800

    用户登录-创建页面组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
 <template>
   <div id="app">
-    <h1>后台管理系统</h1>
+    <!-- <h1>后台管理系统</h1> -->
     <!-- 路由出口 -->
     <router-view />
-    <el-button>默认按钮</el-button>
-    <el-button type="danger">默认按钮</el-button>
-    <el-input-number
-      v-model="num"
-      @change="handleChange"
-      :min="1"
-      :max="10"
-      label="描述文字"
-    ></el-input-number>
   </div>
 </template>
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login-contianer">登录页面2</div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
